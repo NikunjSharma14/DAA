58b8a9e0c9a8b005016a1ddf85bcbfd75cdf4df5
Nikunj Sharma <109721483+NikunjSharma14@users.noreply.github.com>
2023-04-18 11:51:58 +0530

    Add files via upload

---
+++ insertion1.cpp
@@ -0,0 +1,58 @@
+// //insertion sort
+// #include <iostream>
+// #include<cstdlib>
+// using namespace std;
+
+// // Function to generate a random integer between 1 and 100
+// int rand_int() {
+//     return (rand() % 100) + 1;
+// }
+
+
+// void insertionSort(int arr[], int n)
+// {
+//     int i, j;
+//     for (i = 1; i < n; i++)
+//     {
+//         int var = arr[i]; //Taken first element as sorted
+//         for (j = i - 1; j >= 0; j--)
+//         {
+//             //Making comparisons
+//             if (arr[j] > var)
+//             {
+//                 //Shifting elements
+//                 arr[j + 1] = arr[j];
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+//         arr[j + 1] = var;
+//     }
+
+//     cout << "Array after sorting:" << endl;
+
+//     for (int i = 0; i <= n - 1; i++)
+//     {
+//         //Printing the array after being sorted
+//         cout << arr[i] << " ";
+//     }
+// }
+
+// // int main()
+// // {
+// //     int n
+// //     int array[] = {9, 45, 23, 71, 80, 55,45,89,};
+// //     int size = 100;
+// //     insertionSort(array, size);
+// //     // Generate a random array of integers
+// //     for(int i=0; i<n; i++) {
+// //         arr[i] = rand_int();
+// //     }
+
+    
+// }
+
+
+
